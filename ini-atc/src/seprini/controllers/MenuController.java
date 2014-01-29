16ae51fc6719bf1b5016a933143c3dd377ac2547
mjm540@york.ac.uk <sam@heather.sh>
2014-01-29 18:05:09 +0000

    Added leaderboard creation and (bad, needs improving) display code

--- ini-atc/src/seprini/controllers/MenuController.java
+++ ini-atc/src/seprini/controllers/MenuController.java
@@ -4,6 +4,7 @@ import java.util.HashMap;
 
 import seprini.data.Art;
 import seprini.data.GameDifficulty;
+import seprini.models.LeaderboardEntry;
 import seprini.screens.GameScreen;
 import seprini.screens.MenuScreen;
 
@@ -25,6 +26,7 @@ public final class MenuController extends ChangeListener implements Controller {
 
 	private final Table ui;
 	private final MenuScreen screen;
+	private final Leaderboard lb = new Leaderboard();
 
 	private HashMap<String, TextButton> buttons;
 
@@ -44,6 +46,15 @@ public final class MenuController extends ChangeListener implements Controller {
 
 		buttons = new HashMap<String, TextButton>();
 		addButtons();
+	}
+	
+	/**
+	 * Adds the leaderboard to the screen.
+	 */
+	private void addLeaderboard() {
+		for(LeaderboardEntry l : lb.leaderboardEntries) {
+			ui.add(new Label(l.getName() + ": " + l.getScore(), Art.getSkin())).center();
+		}
 	}
 
 	/**
