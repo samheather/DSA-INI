07a3ce73df850fe0642be8825d3d0b81f50c488c
tapuu <mjm540@york.ac.uk>
2014-02-19 23:21:01 +0000

    Documentation

--- ini-atc/src/seprini/models/Airport.java
+++ ini-atc/src/seprini/models/Airport.java
@@ -5,10 +5,17 @@ import java.util.Stack;
 
 import com.badlogic.gdx.math.Vector2;
 
+/**
+ * Implements an airport
+ * Stores planes which enter the airport, launches planes when told to.
+ * @author mjm540
+ *
+ */
 public class Airport extends Waypoint {
 
 	private Stack<Aircraft> aircraft = new Stack<Aircraft>();
 	private final int airportCapacity = 3;
+
 
 	@Override
 	public boolean handleCollision(Aircraft a) {
@@ -30,30 +37,56 @@ public class Airport extends Waypoint {
 		}
 	}
 
+	/**
+	 * Constructor - sets up with position of airport.
+	 */
 	public Airport() {
 		super(new Vector2(400, 345), false);
 		// TODO Auto-generated constructor stub
 	}
 
+	/**
+	 * Getter - get number of planes currently in the airport.
+	 * @return number of aircraft in airport.
+	 */
 	public int getNumberInAirport() {
 		return aircraft.size();
 	}
 
+	/**
+	 * Returns if able to land a new plane at airport, if there is capacity
+	 * @return boolean if can land
+	 */
 	public boolean canLand() {
 		return getNumberInAirport() < airportCapacity;
 	}
 
+	/**
+	 * Returns if can launch plane from airport, if there are planes stored.
+	 * @return boolean if can launch
+	 */
 	public boolean canLaunch() {
 		return getNumberInAirport() > 0;
 	}
 
+	/**
+	 * Lands a aircraft, given as a parameter.  Directs it to the airport, where it will land.
+	 * @param newAircraft
+	 */
 	public void land(Aircraft newAircraft) {
+		// Create new list of waypoints, containing the Airport only.
 		ArrayList<Waypoint> newFlightPlan = new ArrayList<Waypoint>();
 		newFlightPlan.add(this);
+		// Add the new flightplan to the aircraft's flightplan stack
 		newAircraft.waypoints(newFlightPlan);
 	}
 
+	/**
+	 * Launches an aircraft.
+	 * @return
+	 */
 	public Aircraft launch() {
+		// if not able to launch, return null, to be handled externally
 		if (!canLaunch())
 			return null;
 		return aircraft.pop();
